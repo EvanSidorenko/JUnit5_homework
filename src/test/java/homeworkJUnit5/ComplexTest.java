9b1d1c071c5cb91341deb423ef2ba9de234435eb
Ivan <sidorenko.evan@yandex.ru>
2022-06-09 15:04:43 +0300

    Fix bugs

--- src/test/java/homeworkJUnit5/ComplexTest.java
+++ src/test/java/homeworkJUnit5/ComplexTest.java
@@ -5,37 +5,35 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
+
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
 public class ComplexTest {
-    @DisplayName("Проверка поля поиска на bookvoed.ru c ValueSource")
     @BeforeAll
     static void setUp() {
-        Configuration.holdBrowserOpen = true;
+        Configuration.holdBrowserOpen = false;
         Configuration.baseUrl = "https://www.bookvoed.ru/";
         Configuration.browserSize = "1280x609";
     }
-    @CsvSource ({
-            "The War And The Coming Peace.", "Morris Jastrow",
-            "Gone with the wind", "M.Mitchell"
+
+    @CsvSource({
+            "The War and the Coming Peace, Morris Jastrow Jr.",
+            "Gone with the wind, M.Mitchell"
     })
-    @DisplayName("Проверка поля поиска на bookvoed.ru")
-    @ParameterizedTest
-    void firstBookSearchTest(String testData, String expectedResult) {
+    @ParameterizedTest(name = "Проверка поля поиска на bookvoed.ru : {0}, ожидаем результат: {1}")
+    void bookSearchTest(String testData, String expectedResult) {
         //Presteps
-        Selenide.open("https://www.bookvoed.ru/");
+        Selenide.open(Configuration.baseUrl);
         // Steps
         $("[name=q]").setValue(testData);
-        $(".gu").click();
+        $(".ju").click();
         // Expected result:
-        $$("[data-gtm_book_click_detail]").find(
+        $$(".eE").find(
                 Condition.text(expectedResult))
                 .shouldBe(visible);
     }
--- src/test/java/homeworkJUnit5/ParametrizedTest.java
+++ src/test/java/homeworkJUnit5/ParametrizedTest.java
@@ -5,7 +5,6 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static com.codeborne.selenide.Condition.visible;
@@ -15,37 +14,24 @@ import static com.codeborne.selenide.Selenide.$$;
 @DisplayName("Класс с демо тестами для bookvoed.ru")
 public class ParametrizedTest {
     @DisplayName("Проверка поля поиска на bookvoed.ru c ValueSource")
-    @BeforeAll  static void setUp() {
-        Configuration.holdBrowserOpen = true;
+    @BeforeAll
+    static void setUp() {
+        Configuration.holdBrowserOpen = false;
         Configuration.baseUrl = "https://www.bookvoed.ru/";
         Configuration.browserSize = "1280x609";
     }
 
     @ValueSource(strings = {
-            "The War And The Coming Peace.",
+            "The War And The Coming Peace",
             "Gone with the wind"
     })
-    @DisplayName("Проверка поля поиска на bookvoed.ru")
-    @ParameterizedTest
+    @ParameterizedTest(name = "Проверка поля поиска на bookvoed.ru : {0}")
     void firstBookSearchTest(String testData) {
         //Presteps
-        Selenide.open("https://www.bookvoed.ru/");
+        Selenide.open(Configuration.baseUrl);
         // Steps
         $("[name=q]").setValue(testData);
-        $(".gu").click();
-        // Expected result:
-        $$("[data-gtm_book_click_detail]").find(
-                Condition.text(testData))
-                .shouldBe(visible);
-    }
-    void secondBookSearchTest(String testData) {
-        //Presteps
-        Selenide.open("https://www.bookvoed.ru/");
-
-        // Steps
-        $("[name=q]").setValue(testData);
-        $(".gu").click();
-
+        $(".ju").click();
         // Expected result:
         $$("[data-gtm_book_click_detail]").find(
                         Condition.text(testData))
